5773c5ee17357785faac6769b679a92685a476cf
Minamototennsai <Minamototennsai@gmail.com>
2024-05-21 14:59:02 +0800

    v0.1

---
+++ src/main/java/com/ecnudbcourse/acnhdb/HousewareApplication.java
@@ -0,0 +1,12 @@
+package com.ecnudbcourse.acnhdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousewareApplication.class, args);
+	}
+}
